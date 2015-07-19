1a493d534a3e2020f98cc943b811452ce6e545fe
Alex Cordonnier <alexjcord@gmail.com>
2015-07-19 13:39:19 -0700

    Improved ping test

--- connect.js
+++ connect.js
@@ -1,10 +1,12 @@
 var ws;
 var lights;
 
-$(document).ready(function() {
+function connectSocket() {
     ws = new WebSocket("ws://aurora.local:7446", "nlcp");
 
     ws.onopen = function() {
+        $("#connection-status").text("connected");
+        $("#connect-socket").text("Disconnect");
         ws.send("list");
         ws.onmessage = function(evt) {
             if (evt.data.indexOf("Error") == -1) {
@@ -18,8 +20,12 @@ $(document).ready(function() {
     };
 
     ws.onclose = function() {
-        console.log("Connection closed");
+        $("#connection-status").text("not connected");
     };
+}
+
+$(document).ready(function() {
+    connectSocket();
 });
 
 $("#test-lights").on("click", function() {
@@ -30,8 +36,18 @@ $("#test-lights").on("click", function() {
     }
 });
 
+$("#connect-socket").on("click", function() {
+    if (ws.readyState == WebSocket.OPEN) {
+        ws.close();
+        $("#connect-socket").text("Connect");
+    } else {
+        connectSocket();
+    }
+})
+
 $("#ping-test").on("click", function() {
-    testPing(10, 1000, function(results) {
+    $("#ping-result, #packet-loss-result").text("testing...");
+    testPing(50, 100, function(results) {
         $("#ping-result").text(results.avg.toFixed(3) + " ms");
         $("#packet-loss-result").text((results.loss*100).toFixed(1) + "%");
     });
@@ -42,6 +58,9 @@ var transmitted = 0;
 var received = [];
 var timeoutHandle;
 function testPing(numTests, timeout, callback) {
+    received = [];
+    transmitted = 0;
+
     ws.onmessage = function(evt) {
         var start = parseFloat(evt.data.replace("ping ", ""));
         var latency = window.performance.now() - start;
@@ -54,47 +73,7 @@ function testPing(numTests, timeout, callback) {
             // Start the next ping
             ping();
         } else {
-            // Calculate stats
-            function average(data) {
-                var sum = data.reduce(function(sum, value) {
-                    return sum + value;
-                }, 0);
-
-                var avg = sum / data.length;
-                return avg;
-            }
-
-            var min = Math.min.apply(Math, received);
-            var avg = average(received);
-            var max = Math.max.apply(Math, received);
-            var squareDiffs = received.map(function(value){
-                var diff = value - avg;
-                var sqr = diff * diff;
-                return sqr;
-            });
-            var stddev = average(squareDiffs);
-            var loss = (transmitted - received.length)/transmitted;
-
-            // Print stats
-            console.log("--- Ping statistics ---");
-            console.log("%d messages transmitted, %d messages received, %s% packet loss",
-                transmitted,
-                received.length,
-                (loss*100).toFixed(1));
-            console.log("round-trip min/avg/max/stddev = %s/%s/%s/%s ms",
-                min.toFixed(3),
-                avg.toFixed(3),
-                max.toFixed(3),
-                stddev.toFixed(3));
-
-            // Alert the caller of the data
-            callback({
-                min: min,
-                avg: avg,
-                max: max,
-                stddev: stddev,
-                loss: loss
-            });
+            printStats();
         }
     }
 
@@ -104,7 +83,64 @@ function testPing(numTests, timeout, callback) {
         transmitted++;
         timeoutHandle = setTimeout(function() {
             console.log("Request timeout");
+            numTests--;
+            if (numTests) {
+                ping();
+            } else {
+                printStats();
+            }
         }, timeout);
+    }
+
+    function printStats() {
+        // Calculate stats
+        function average(data) {
+            var sum = data.reduce(function(sum, value) {
+                return sum + value;
+            }, 0);
+
+            var avg = sum / data.length;
+            return avg;
+        }
+
+        var min = Math.min.apply(Math, received);
+        var avg = average(received);
+        var max = Math.max.apply(Math, received);
+        var squareDiffs = received.map(function(value){
+            var diff = value - avg;
+            var sqr = diff * diff;
+            return sqr;
+        });
+        var stddev = average(squareDiffs);
+        var loss = (transmitted - received.length)/transmitted;
+
+        if (received.length == 0) {
+            min = Infinity;
+            max = Infinity;
+            avg = Infinity;
+            stddev = Infinity;
+        }
+
+        // Print stats
+        console.log("--- Ping statistics ---");
+        console.log("%d messages transmitted, %d messages received, %s% packet loss",
+            transmitted,
+            received.length,
+            (loss*100).toFixed(1));
+        console.log("round-trip min/avg/max/stddev = %s/%s/%s/%s ms",
+            min.toFixed(3),
+            avg.toFixed(3),
+            max.toFixed(3),
+            stddev.toFixed(3));
+
+        // Alert the caller of the data
+        callback({
+            min: min,
+            avg: avg,
+            max: max,
+            stddev: stddev,
+            loss: loss
+        });
     }
 
     ping();
